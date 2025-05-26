efcfb567619f9704c65efb4a81d9de9608cf644b
DanMS98 <d.moradisabzevar@gmail.com>
2025-05-26 17:08:04 +0300

    added first version of lidar transform to base_link

--- source/sensor_sync/sensor_sync/calibration_node.py
+++ source/sensor_sync/sensor_sync/calibration_node.py
@@ -17,9 +17,14 @@ import matplotlib.pyplot as plt
 from tf2_ros import Buffer, TransformListener
 from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
 import tf2_ros
+from sensor_msgs_py.point_cloud2 import create_cloud, read_points, create_cloud_xyz32
 from geometry_msgs.msg import TransformStamped
 import time
 from scipy.spatial.transform import Rotation as R
+from geometry_msgs.msg import Point
+import PyKDL
+# from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud
+
 
 
 class CalibrationNode(Node):
@@ -39,27 +44,10 @@ class CalibrationNode(Node):
             10
         )
         self.lidar_pub = self.create_publisher(PointCloud2, '/ouster/points_base', 10)
+        self.publisher = self.create_publisher(PointCloud2, 'transformed_cloud', 10)
 
         logger.info("Calibration Node Initialized")
 
-        # self.subscription = self.create_subscription(
-        #     SyncedSensors,
-        #     '/synced_sensors',
-        #     self.listener_callback,
-        #     10
-        # )
-
-        
-
-        # self.l_utils = LidarUtils()
-        # # self.sync_pub = self.create_publisher(SyncedSensors, '/synced_sensors', 10)
-        # # self.timer = self.create_timer(0.5, self.timer_callback)
-
-        # self.camera = None
-        # self.lidar = None
-        # self.radar = None
-        # self.visualize = False
-        # self.bridge = CvBridge()
 
     def lookup_all_transforms(self):
         self.lookup_transform('os_lidar', 'base_link')
@@ -80,66 +68,61 @@ class CalibrationNode(Node):
                 logger.info(f'Rotation: {transform.transform.rotation}')
         except Exception as e:
             logger.error(f'Failed to get transform: {e}')
-        return self.transform_to_matrix(transform)
+        return transform
+    
 
-    def transform_to_matrix(self, transform):
-        t = transform.transform.translation
-        q = transform.transform.rotation
+    # def transform_to_matrix(self, transform):
+    #     t = transform.transform.translation
+    #     q = transform.transform.rotation
 
-        translation = np.array([t.x, t.y, t.z])
-        rotation = R.from_quat([q.x, q.y, q.z, q.w]).as_matrix()
+    #     translation = np.array([t.x, t.y, t.z])
+    #     rotation = R.from_quat([q.x, q.y, q.z, q.w]).as_matrix()
 
-        T = np.eye(4)
-        T[:3, :3] = rotation
-        T[:3, 3] = translation
-        return T
+    #     T = np.eye(4)
+    #     T[:3, :3] = rotation
+    #     T[:3, 3] = translation
+    #     return T
+
+    def transform_to_kdl(self, t):
+        return PyKDL.Frame(PyKDL.Rotation.Quaternion(
+                            t.transform.rotation.x, t.transform.rotation.y,
+                            t.transform.rotation.z, t.transform.rotation.w),
+                        PyKDL.Vector(t.transform.translation.x,
+                                    t.transform.translation.y,
+                                    t.transform.translation.z))
+    
+    def do_transform_cloud(self, cloud, transform):
+        t_kdl = self.transform_to_kdl(transform)
+        points_out = []
+        for p_in in read_points(cloud, field_names=("x", "y", "z")):
+            p_out = t_kdl * PyKDL.Vector(p_in[0], p_in[1], p_in[2])
+            points_out.append([p_out[0], p_out[1], p_out[2]])
+        res = create_cloud_xyz32(transform.header, points_out)
+        return res
+            
 
 
     def listener_callback(self, msg: PointCloud2):
-        # pc_np = rnp.numpify(msg)
-        # pc_xyz = rnp.get_xyz_points(pc_np, remove_nans=True)
-        # points = list(pc2.read_points(msg,
-        #                                skip_nans=True,
-        #                                  field_names=None))
-        points = pc2.read_points(
-            msg,
-            field_names=("x", "y", "z"),
-            skip_nans=True)
-        if len(points) == 0:
-            logger.error("No valid LiDAR points to transform.")
-            return
+
         
-        T = self.lookup_transform(source_frame=msg.header.frame_id,
+        tf_msg = self.lookup_transform(source_frame=msg.header.frame_id,
                               target_frame='base_link', print_tfs=True)
-        if T is None or T.shape != (4, 4):
-            logger.error(f"Invalid transformation matrix: {T}")
-            return
-        
-        xyz = np.array([[p[0], p[1], p[2]] for p in points])
-        num_points = xyz.shape[0]
-        homo_xyz = np.hstack((xyz, np.ones((num_points, 1))))
-        transformed_xyz = (T @ homo_xyz.T).T[:, :3]
-        logger.debug(f'transformed_xyz:{transformed_xyz}')
-        # reconstruct 
-        field_names = [f.name for f in msg.fields]
-        full_dtype = pc2.PointCloud2(msg).fields
-        fields = msg.fields
+        # T = self.transform_to_matrix(tf_msg)
+        transformed_cloud = self.do_transform_cloud(msg, tf_msg)
+        logger.debug('transformed succedfully')
+        logger.info(transformed_cloud.header)
+        self.publisher.publish(transformed_cloud)
 
-        transformed_points = []
-        for i, p in enumerate(points):
-            p = list(p)
-            p[0], p[1], p[2] = transformed_xyz[i]
-            transformed_points.append(tuple(p))
-
-        # newPointCloud2
-        transformed_msg = pc2.create_cloud(
-            header=msg.header,
-            fields=fields,
-            points=transformed_points
-        )
-        transformed_msg.header.frame_id = 'base_link'
-        self.lidar_pub.publish(transformed_msg)
-        logger.debug("Published transformed LiDAR point cloud")
+            
+        # if T is None or T.shape != (4, 4):
+        #     logger.error(f"Invalid transformation matrix: {T}")
+        #     return
+        ################
+        # transformed_cloud = do_transform_cloud(msg, tf_msg)
+        # transformed_cloud.header.frame_id = 'base_link'
+        ################
+     
+       
 
 
 
